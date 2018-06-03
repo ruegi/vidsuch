b8475088e8de813e346d3f6f9bc16c56d20da50e
ruegi <rg@rgilbert.de>
2018-06-03 14:12:27 +0200

    Version 1.4 Die such-Funktion mit 2 Suchbegriffen läuft jetzt besser

--- suchtest.py
+++ suchtest.py
@@ -14,44 +14,57 @@ def findewas(such, such2, ausgabefkt):
     '''
     Findet Videos nach Stichworten
     sucht unabhängig von Groß/klein-schreibung
+    enthält der erste suchbegriff "such" eine Blank, so wird auch nach "_" an dieser Stelle gesucht
     :param such:        Suchbegriff 1
     :param such2:       Suchbegriff 2
     :param ausgabefkt:  None oder der Name einer Funktion, um den letzten gefundenen EIntrag zu zeigen
     :return:            Liste mit den vollen Namen der gefundenen Videos
     '''
     such = such.lower()
+    repl_mode = False
+    if " " in such or "_" in such:
+        such_ = such.replace(" ", "_")      # such1 = such, aber komplett mit _ statt Blank
+        suchb = such.replace("_", " ")       # such wie such, aber komplett mit Blank (kein _)
+        repl_mode = True
     if such2 is None:
         doSuch2 = False
     else:
         such2 = such2.lower()
         doSuch2 = True
     lst = []
-    lst2 = []
     for root, dirs, files in os.walk(ziel):
         for f in files:
             fl = f.lower()
-            if such in fl:
-                x = os.path.join(root, f)
+            x = None
+            if repl_mode:
+                if suchb in fl or such_ in fl:
+                    if doSuch2:
+                        if such2 in fl:
+                            x = os.path.join(root, f)
+                    else:
+                        x = os.path.join(root, f)
+            else:
+                if such in fl:
+                    if doSuch2:
+                        if such2 in fl:
+                            x = os.path.join(root, f)
+                    else:
+                        x = os.path.join(root, f)
+            if x is None:
+                continue
+            else:
                 lst.append(x)
-    if doSuch2:
-        for f in lst:
-            fl = f.lower()
-            if such2 in fl:
-                lst2.append(f)
-                if not ausgabefkt is None:
-                    ausgabefkt(f)
-        return lst2
-    else:
-        return lst
+    return lst
 
-such = "X-Men"
-such2 = None    #"Beyond"
-
+such = "Inspector Barnaby"
+such2 = "Dame"
+# such2 = None
 print("Suche...", end=" ")
 erg = findewas(such, such2, None)
-print("OK")
+print("Fertig")
+
 if erg == []:
-    print("Nicht gefunden")
+    print("Nichts gefunden")
 else:
     for f in erg:
         print(f)
