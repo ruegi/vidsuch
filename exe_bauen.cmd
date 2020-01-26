4f7b37a8eb14d23b5a91eca4255bba9da14fb62c
RuegGi <rg@rgilbert.de>
2020-01-26 18:47:09 +0100

    Wiederaufnahme der Git-Entwicklung

--- exe_bauen.cmd
+++ exe_bauen.cmd
@@ -1,10 +1,11 @@
-pyuic5 -x VidSuchUI3.ui -o VidSuchUI3.py
-goto Ende
+pyuic5 -x VidSuchUI4.ui -o VidSuchUI.py
+rem goto Ende
 
 del /S .\dist\VidSuch
-pyinstaller -w -i VidSuch.ico --clean VidSuch.py
+pyinstaller -w -i VidSuch.ico --clean VidSuch4.py
 copy .\VidSuch.ico .\dist\VidSuch
-
+copy .\EndBut*.png .\dist\
 :Ende
 echo Fertig!
 rem pause
+	
